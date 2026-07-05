6c622279022d368c0bb22967ccfb6df9fafb7bc9
Vangala Sreeja <vangalasreeja88@gmail.com>
2026-07-05 11:07:37 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function () {
+    let navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
